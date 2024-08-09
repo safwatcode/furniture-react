b086b89561e8ab923082aeebe3bf6590e9a2c728
Safwat Ahmed <safwatahmed669@gmail.com>
2024-08-10 02:32:12 +0300

    edited products

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Product from "./Product";
+import products from "../data/products";
 
-const ProductList = ({ products }) => {
+const ProductList = () => {
   return (
     <Container className="my-5">
-      <Row className="w-full d-flex justify-content-around flex-wrap">
+      <Row className="d-flex justify-content-center">
         {products.map((product) => (
           <Col
             key={product.id}
             sm={12}
             md={6}
-            lg={4}
+            lg={3}
             className="mb-4 d-flex justify-content-center"
           >
             <Product product={product} />
